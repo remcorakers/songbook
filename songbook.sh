da7595de538c8c641bcca2f9edfcb305f71b6f99
Remco Rakers <remcorakers@gmail.com>
2019-04-22 12:15:06 +0200

    Add support for Docker

--- songbook.sh
+++ songbook.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
 if [ "$1" == "" ]; then
-    echo "Please provide an path where your tab files are located."
+    echo "Please provide a path where your tab files are located."
     exit 4
 fi
 
-tabDir=${1%/} # remove last slash, if existing
-
-escapedDir="${tabDir//\//\/}"
-expression="%s/TABDIR/$escapedDir/g"
-
 # use a temp file, based on songbook.tex
-cp ./songbook.tex ./songbook.temp.tex
+cp ./songbook.tex ./output/songbook.temp.tex
 
 # replace TABDIR placeholder with actual value of the directory with tab files
-ex -sc $expression -cx ./songbook.temp.tex
+tabDir=${1%/} # remove last slash, if existing
+sed -i "s|TABDIR|$tabDir|g" ./output/songbook.temp.tex
 
 # run twice: first run for the table of contenst, second one for complete PDF
-xelatex -enable-write18 "./songbook.temp.tex"
-xelatex -enable-write18 "./songbook.temp.tex"
+xelatex -enable-write18 -output-directory=./output "./output/songbook.temp.tex"
+xelatex -enable-write18 -output-directory=./output "./output/songbook.temp.tex"
 
 # rename generated songbook pdf
-mv ./songbook.temp.pdf songbook.pdf
+mv ./output/songbook.temp.pdf ./output/songbook.pdf
 
 # clean up temp files
-rm ./*temp*
+rm ./output/*temp*
